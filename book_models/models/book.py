f0e0935fbda6f46b8ea469c3985f76a36de75134
Adrian Moloce <adrian.moloce@complyadvantage.com>
2019-07-06 09:59:59 +0300

    Decoupled the book model since the users won't access it.

--- book_models/models/book.py
+++ book_models/models/book.py
@@ -1,23 +1,31 @@
 """This module encapsulates models regarding the a book object."""
 from datetime import datetime
+from typing import Optional
 from uuid import uuid4
-
-from book_models.models.user import User
 
 
 class Book:
     """Basic model for a book."""
-    def __init__(self, author: User, title: str, genre: str, publication_date: datetime):
+    def __init__(
+            self,
+            author: str,
+            title: str,
+            genre: Optional[str] = None,
+            publisher: Optional[str] = None,
+            publication_date: Optional[datetime] = None
+    ):
         """Initializer.
 
         Args:
             author: User - the author of the book
             title: str - title of the book
             genre: str - the genre of the book
+            publisher: str - The name of the publishing unit
             publication_date: datetime - the date when the book was published
         """
         self._book_id = uuid4()
         self.author = author
         self.title = title
         self.genre = genre
+        self.publisher = publisher
         self.publication_date = publication_date
